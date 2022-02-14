94a22d85b8a90109d5565ae0773bc5c986019d79
Ville Saari <v.j.saari@gmail.com>
2022-02-14 15:44:59 +0200

    moved config to separate module

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('dotenv').config()
 const logger = require("./utils/logger")
+const config = require('./utils/config')
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -15,8 +15,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -39,7 +38,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
